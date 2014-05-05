2e7d627f667fb62d59d960718401bb8b3d27e59e
gezhu <zhulianhua121@qq.com>
2014-05-05 01:45:48 -0400

    add post fortran function pointers

--- _posts/2014-05-05-fortran-functoin-pointer.f90
+++ _posts/2014-05-05-fortran-functoin-pointer.f90
@@ -10,10 +10,48 @@ tags:
 ---
 例子:
 
-    {% highlight fortran %}
-    call etime(tarray,t0)
-    ! ... ...
-    call etime(tarray,t1)
+{% highlight fortran %}
+module ExampleFuncs
+   implicit none
+    contains
+    function f1 (x)
+      real :: f1
+      real, intent (in) :: x
+      f1 = 2.0 * x
+      write(*,*) "I am func 1"
+    end function f1
 
-    print *, 'Elapsed time:', t1 - t0
-    {% endhighlight %}
+    function f2 (x)
+       real :: f2
+       real, intent (in) :: x
+       f2 = 3.0 * x**2
+       write(*,*) "I am func 2"
+       return
+    end function f2
+end module ExampleFuncs
+
+
+program test_func_ptrs
+    use ExampleFuncs
+    implicit none
+    abstract interface
+        function func (z)
+            real :: func
+            real, intent (in) :: z
+        end function func
+    end interface
+
+   procedure (func), pointer :: f_ptr => null ()
+   real :: input
+
+   write (*, '( / "Input test value: ")', advance="no" )
+   read (*, *) input
+   if ( input < 0 ) then
+      f_ptr => f1
+   else
+      f_ptr => f2
+   end if
+   write (*, '(/ "evaluate function: ", ES14.4 )' )  f_ptr (input)
+   stop
+end program test_func_ptrs
+{% endhighlight %}
